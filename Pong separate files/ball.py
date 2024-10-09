f22dd84efb1ea5952b29080cf7b2e6eb67085ce0
PJacobs01 <PJACOBS@acas.org.uk>
2024-10-09 11:20:21 +0100

    Ball file created. Ball variable created

---
+++ Pong separate files/ball.py
@@ -0,0 +1,8 @@
+import pygame, sys
+from settings import width, height
+
+ball = pygame.Rect(0,0,30,30)
+ball.center = (width/2, height/2)
+
+
+#First 2 values = coordinates, Last 2 values = dimensions in pixels
