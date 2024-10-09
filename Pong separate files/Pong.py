d7fd71b9067d75f425ec237783c5487706bca969
PJacobs01 <PJACOBS@acas.org.uk>
2024-10-09 14:06:52 +0100

    Paddles created. CPU and player. Struggling with movement.

--- Pong separate files/Pong.py
+++ Pong separate files/Pong.py
@@ -4,20 +4,35 @@ pong module
 __version__="0.1.0"
 import pygame, sys
 from settings import width, height
+import ball
+import random
 
 pygame.init()
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Pong")
-
 clock = pygame.time.Clock()
 #^Screen set up, title and game clock added
-# --------------------------------------------------
+#------------------------------------------------------
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 #^Command lines (while loop) for exiting the game - if select quit then programme closes. Checking for events.
+
+    #screen.fill('black')
+    # Drawing objects
+    pygame.draw.aaline(screen, 'white', (width / 2, 0), (width / 2, height))
+    pygame.draw.ellipse(screen, 'white', ball.ball)
+    # ^1st, where to draw object, 2nd is colour, 3rd define of rect (pulled from ball .py) .ellipse = circle
+    pygame.draw.rect(screen, 'red', ball.player)
+    # Player paddle + position
+    pygame.draw.rect(screen, 'white', ball.cpu)
+    # CPU paddle + position
+    def _random_direction(self):
+        direction = ("right", "left")
+        self.direction = random.choice(direction)
 
     pygame.display.update()
     clock.tick(60)
