5eec8cdb148c0baa5fa19a80eda98b05e4ead002
PJacobs01 <PJACOBS@acas.org.uk>
2024-10-09 14:08:16 +0100

    ball function added but will probably be removed. for purposes of discussion with Phil.
    Pads position has been added too.

--- Pong separate files/ball.py
+++ Pong separate files/ball.py
@@ -1,10 +1,18 @@
-import pygame, sys
+import pygame
 from settings import width, height, player_height, player_width
 
+#Ball
 ball = pygame.Rect(width/2,height/2,30,30)
+def _random_direction(self):
+    direction = ("right", "left")
+    self.direction = random.choice(direction)
 
-player = pygame.Rect(880,0, player_width, player_height)
-cpu = pygame.Rect(0,0, player_width, player_height)
+#Player Pad
+player = pygame.Rect(0,0, player_width, player_height)
+player.centery = height/2
 
-
+#CPU Pad
+cpu = pygame.Rect(880,0, player_width, player_height)
+cpu.centery = height/2
 #First 2 values = coordinates, Last 2 values = dimensions in pixels
+
