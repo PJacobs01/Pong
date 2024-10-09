4ece9535307a1dc6360371ab2fa764f31b555062
PJacobs01 <PJACOBS@acas.org.uk>
2024-10-09 11:47:54 +0100

    Paddles created. CPU and player.

--- Pong separate files/ball.py
+++ Pong separate files/ball.py
@@ -1,8 +1,10 @@
 import pygame, sys
-from settings import width, height
+from settings import width, height, player_height, player_width
 
-ball = pygame.Rect(0,0,30,30)
-ball.center = (width/2, height/2)
+ball = pygame.Rect(width/2,height/2,30,30)
+
+player = pygame.Rect(880,0, player_width, player_height)
+cpu = pygame.Rect(0,0, player_width, player_height)
 
 
 #First 2 values = coordinates, Last 2 values = dimensions in pixels
