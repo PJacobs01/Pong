e70e8d0d9eefc8d75742a051866eff1c682ceb1d
PJacobs01 <PJACOBS@acas.org.uk>
2024-10-09 15:25:24 +0100

    removal of player details

--- Pong separate files/ball.py
+++ Pong separate files/ball.py
@@ -6,12 +6,5 @@ ball = pygame.Rect(width/2,height/2,30,30)
 ball_speed_x = 6
 ball_speed_y = 6
 
-#Player Pad
-player = pygame.Rect(0,0, player_width, player_height)
-player.centery = height/2
 
-#CPU Pad
-cpu = pygame.Rect(880,0, player_width, player_height)
-cpu.centery = height/2
-#First 2 values = coordinates, Last 2 values = dimensions in pixels
 
