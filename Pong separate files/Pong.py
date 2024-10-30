55d3b6f651bad4a9ba39f20acaf876e2e8f93b49
PJacobs01 <PJACOBS@acas.org.uk>
2024-10-30 17:28:14 +0000

    added draw_menu, menu_selection, start_game(difficulty) functions for selection menu

--- Pong separate files/Pong.py
+++ Pong separate files/Pong.py
@@ -7,14 +7,71 @@ from settings import width, height
 import ball as b
 import player as p
 import random
+random.seed()
 
 pygame.init()
 screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Pong")
+pygame.display.set_caption("Select Difficulty")
 clock = pygame.time.Clock()
+font = pygame.font.SysFont(None, 50)
+difficulties = ["Easy", "Medium", "Hard"]
+difficulty_rects = []
 
 #^Screen set up, title and game clock added
 #------------------------------------------------------
+
+def draw_menu():
+    screen.fill('black')
+    y_offset = 150
+    for index, difficulty in enumerate(difficulties):
+        text = font.render(difficulty,True, 'white')
+        rect = text.get_rect(center=(width//2, y_offset))
+        difficulty_rects.append((rect, difficulty))
+        screen.blit(text,rect)
+        y_offset +=100
+
+    pygame.display.update()
+
+def menu_selection():
+    while True:
+        draw_menu()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = event.pos
+                for rect, difficulty in difficulty_rects:
+                    if rect.collidepoint(mouse_pos):
+                        return difficulty
+
+def start_game(difficulty):
+    if difficulty == "Easy":
+        cpu_speed = 3.75
+        if b.ball.centery < p.cpu.centery - 15:
+            p.cpu.y -= cpu_speed
+        elif b.ball.centery > p.cpu.centery + 15:
+            p.cpu.y += cpu_speed
+    elif difficulty == "Medium":
+        cpu_speed = 4.5
+        if b.ball.centery < p.cpu.centery :
+            p.cpu.y -= cpu_speed
+        elif b.ball.centery > p.cpu.centery :
+            p.cpu.y += cpu_speed
+        """if random.random() < 0.1:
+            p.cpu.centery += random.choice ([-cpu_speed, cpu_speed])"""
+    elif difficulty == "Hard":
+        cpu_speed = 5
+        if b.ball.centery < p.cpu.centery:
+            p.cpu.y -= cpu_speed
+        elif b.ball.centery > p.cpu.centery:
+            p.cpu.y += cpu_speed
+        """p.cpu.centery = b.ball.centery + b.ball_speed_x * 2
+        if p.cpu.top < 0:
+            p.cpu.top = 0
+        if p.cpu.bottom > height:
+            p.cpu.bottom = height"""
+
 def ball_movement():
     b.ball.x += b.ball_speed_x
     b.ball.y += b.ball_speed_y
@@ -30,21 +87,21 @@ def ball_movement():
         b.ball_speed_x *= -1.1
 """
 def speed_up():
-    delta = int(p.cpu_points + p.player_points) // 10
+    delta = int(p.cpu_points + p.player_points) //5
     if b.ball_speed_x > 0:
         b.ball_speed_x += delta
-    else:
-        b.ball_speed_x -= delta
+    #else:
+     #   b.ball_speed_x -= delta
     if b.ball_speed_y > 0:
         b.ball_speed_y += delta
-    else:
-        b.ball_speed_y -= delta
-        """
+   # else:
+     #   b.ball_speed_y -= delta
+"""
 
 def reset_ball():
     b.ball.x = width / 2 - 10
     b.ball.y = random.randint(10,100)
-    b.ball_speed_x = b.original_speed
+    b.ball_speed_x *= random.choice([6,-6])
 
 def player_movement():
     p.player.y += p.player_speed
@@ -52,18 +109,18 @@ def player_movement():
         p.player.top = 0
     if p.player.bottom >= height:
         p.player.bottom = height
-
+"""
 def cpu_movement():
     p.cpu.y += p.cpu_speed
     if b.ball.centery <= p.cpu.centery:
-        p.cpu_speed =  -6
+        p.cpu_speed = -4.5
     if b.ball.centery >= p.cpu.centery:
-        p.cpu_speed = 6
-
+        p.cpu_speed = 4.5
     if p.cpu.top <= 0:
         p.cpu.top = 0
     if p.cpu.bottom >= height:
         p.cpu.bottom = height
+"""
 
 def display_winner(screen, winner):
     if winner == "cpu":
@@ -90,12 +147,13 @@ def point_won(winner):
         reset_game()
 
 def reset_game():
-    p.cpu_points, p.player_points
     p.cpu_points = 0
     p.player_points = 0
     reset_ball()
 
-
+difficulty = menu_selection()
+cpu_speed = start_game(difficulty)
+pygame.display.set_caption("Pong")
 
 while True:
     for event in pygame.event.get():
@@ -115,9 +173,10 @@ while True:
                 p.player_speed = 0
 
 #^Command lines (while loop) for exiting the game - if select quit then programme closes. Checking for events.
+    start_game(difficulty)
     ball_movement()
     player_movement()
-    cpu_movement()
+    #cpu_movement()
     #speed_up()
     score_font = pygame.font.Font(None, 100)
 
