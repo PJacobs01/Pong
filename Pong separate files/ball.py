0ccd8b7c48488695629cbdf3565fafc498907f56
PJacobs01 <PJACOBS@acas.org.uk>
2024-11-05 15:52:11 +0000

    ball speed reset given randomization to travel left or right at reset.

--- Pong separate files/ball.py
+++ Pong separate files/ball.py
@@ -4,9 +4,10 @@ import random
 
 #Ball
 ball = pygame.Rect(width/2,height/2,30,30)
-ball_speed_x = 6
-ball_speed_y = 6
+ball_speed_x = random.choice([-6,6])
+ball_speed_y = random.choice([-6,6])
 
-original_speed = 6
+
+original_speed = random.choice([-6,6])
 
 
